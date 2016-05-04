d9c8eb1a81114bb41c6973c0e693b92bdd3b7489
Kartik Mohta <kartikmohta@gmail.com>
2016-05-04 00:06:01 -0400

    Fixes for OpenCV3

--- include/rovio/ImagePyramid.hpp
+++ include/rovio/ImagePyramid.hpp
@@ -134,8 +134,8 @@ class ImagePyramid{
    */
   void detectFastCorners(std::vector<FeatureCoordinates>& candidates, int l, int detectionThreshold) const{
     std::vector<cv::KeyPoint> keypoints;
-    cv::FastFeatureDetector feature_detector_fast(detectionThreshold, true);
-    feature_detector_fast.detect(imgs_[l], keypoints);
+    auto feature_detector_fast = cv::FastFeatureDetector::create(detectionThreshold, true);
+    feature_detector_fast->detect(imgs_[l], keypoints);
     FeatureCoordinates c;
     candidates.reserve(candidates.size()+keypoints.size());
     for (auto it = keypoints.cbegin(), end = keypoints.cend(); it != end; ++it) {
